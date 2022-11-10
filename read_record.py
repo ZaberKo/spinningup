73cecfc5993b68625ffa04b34a4a345ad0f3b805
zaber <20830726+ZaberKo@users.noreply.github.com>
2022-11-10 10:16:20 +0000

    remove unnecessary deps in read_record

--- read_record.py
+++ read_record.py
@@ -1,15 +1,5 @@
 # %%
-import matplotlib.pyplot as plt
-import pandas as pd
-import os
-import numpy as np
-
-from pathlib import Path
-
-
 from record_utils import plot, plot_all, plot_all_alpha
-
-
 
 
 # %%
